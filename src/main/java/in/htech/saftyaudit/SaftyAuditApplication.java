20efc92b79592a0ec590515bbd6ad7c11775c48d
Harshad Wazarkar <harshad_wazarkar@vistasoftwares.com>
2019-04-17 16:29:24 +0530

    Add project stracture

---
+++ src/main/java/in/htech/saftyaudit/SaftyAuditApplication.java
@@ -0,0 +1,13 @@
+package in.htech.saftyaudit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaftyAuditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaftyAuditApplication.class, args);
+	}
+
+}
